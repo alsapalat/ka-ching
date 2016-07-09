5a66f816304b9ae37f0120b744e2bd25987b0ba0
Alee <al.s.apalat@gmail.com>
2016-07-09 23:37:43 +0800

    API: User CRUD

--- api/src/app/rest.js
+++ api/src/app/rest.js
@@ -8,12 +8,12 @@ function REST_ROUTER(router,connection,md5) {
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     
     router.get("/",function(req,res){
-        res.json({"Message" : "Connection OK..."});
+        res.json({"Message" : "Connection Working..."});
     });
 
-    //POST 
+    //CREATE USER
     router.post("/users", function(req, res){
-    	var query = "INSERT INTO ??(??,??) VALUES (?.?)";
+    	var query = "INSERT INTO ??(??,??) VALUES (?,?)";
     	var table = [
     		"users",
     		"user_email",
@@ -21,23 +21,100 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     		req.body.email, 
     		md5(req.body.password)
 		];
-
 		query = mysql.format(query, table);
-
 		connection.query(query, function(err, rows){
 			if(err){
 				res.json({
 					"error" : true,
-					"message" : "Error Executing MySql Query"
+					"message" : "Error | " + err
 				});
 			} else {
 				res.json({
 					"error" : false,
-					"message" : "User Added!"
+					"message" : "User has been Added!"
 				});
 			}
 		});
     });
+
+    //READ USER
+    router.get("/users", function(req, res){
+    	var query = "SELECT * FROM ??";
+    	var table = ["users"];
+    	query = mysql.format(query, table);
+    	connection.query(query, function(err, rows){
+    		if(err){
+				res.json({
+					"error" : true,
+					"message" : "Error | " + err
+				});
+    		} else {
+    			res.json({
+    				"error" : false,
+    				"message" : "Sucess", "Users" : rows
+    			});
+    		}
+    	});
+    });
+
+    router.get("/users/:id", function(req, res){
+    	var query = "SELECT * FROM ?? WHERE ??=?";
+    	var table = ["users","user_id", req.params.id];
+    	query = mysql.format(query, table);
+    	connection.query(query, function(err, rows){
+    		if(err){
+				res.json({
+					"error" : true,
+					"message" : "Error | " + err
+				});
+    		} else {
+    			res.json({
+    				"error" : false,
+    				"message" : "Sucess", "Users" : rows
+    			});
+    		}
+    	});
+    });
+
+    //UPDATE USER
+    router.put("/users/changepassword/:id", function(req, res){
+    	var query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+    	var table = ["users", "user_password", md5(req.body.password), "user_id", req.params.id];
+    	query = mysql.format(query,table);
+    	connection.query(query, function(err,rows){
+    		if(err){
+				res.json({
+					"error" : true,
+					"message" : "Error | " + err
+				});
+    		} else {
+    			res.json({
+    				"error" : false,
+    				"message" : "User " + req.params.id + "'s password has been changed..."
+    			});
+    		}
+    	})
+    });
+
+    //DELETE USER
+    router.delete("/users/:email", function(req, res) {
+    	var query = "DELETE from ?? WHERE ??=?";
+    	var table = ["users", "user_email", req.params.email];
+    	query = mysql.format(query,table);
+    	connection.query(query, function(err,rows){
+    		if(err){
+				res.json({
+					"error" : true,
+					"message" : "Error | " + err
+				});
+    		} else {
+    			res.json({
+    				"error" : false,
+    				"message" : req.params.email + "'s account has been deleted...", "Users": rows
+    			});
+    		}
+    	})
+    })
 }
 
 module.exports = REST_ROUTER;
